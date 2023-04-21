71b5ba2a7340cb243621d5a04ca638be6c39feb9
Nikki <10172904+nix-moo@users.noreply.github.com>
2023-04-21 16:56:54 -0400

    Part 1

---
+++ cmd/cyoaweb/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"cyoa"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "the port to start web app on")
+	file := flag.String("file", "gopher.json", "the JSON file containing the story text")
+	flag.Parse()
+	fmt.Printf("Using the story in %s.\n", *file)
+
+	f, err := os.Open(*file)
+	if err != nil {
+		fmt.Printf("Something went wrong")
+	}
+
+	story, err := cyoa.JsonStory(f)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting server on port %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+}
